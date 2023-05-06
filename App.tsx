ae55898b61a5c73fb075d00c8a0116d0a54e68d2
ivandjoh <ivandi.djoh@gmail.com>
2023-05-07 00:00:53 +0800

    focus on simple styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import FirstComponent from './components/FirstComponent';
 
-export default function App() {
+import {View, Text, useColorScheme, StyleSheet} from 'react-native';
+
+export default function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <SafeAreaView>
-      <FirstComponent />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.darkText : styles.whiteText}>
+        Welcome to React Native!
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', // left to right
+    justifyContent: 'center', // top to bottom
+    backgroundColor: '#000',
+  },
+  whiteText: {
+    fontFamily: 'Roboto, sans-serif',
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  darkText: {
+    fontFamily: 'Roboto, sans-serif',
+    color: '#000000',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
